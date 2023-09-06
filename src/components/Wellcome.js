b98bef9001d59cc52ecba60e519d1dac59cde2d4
oscar50 <oscar@whaleandjaguar.co>
2023-09-05 20:40:20 -0500

    feat: useState

--- src/components/Wellcome.js
+++ src/components/Wellcome.js
@@ -1,11 +1,17 @@
+import { useState } from "react";
+
 export default function Wellcome(props) {
-  console.log(props);
+  const [counter, setCounter] = useState(0);
   const { message, name } = props;
 
   return (
     <div>
       <h1>Hola, {name}</h1>
-      <p>{message}</p>
+      <h2>Contador de React con Hooks</h2>
+      <h4>El numero del contador es: {counter}</h4>
+      <button type="submit" onClick={() => setCounter(counter + 1)}>
+        Sumar Contador
+      </button>
     </div>
   );
 }
